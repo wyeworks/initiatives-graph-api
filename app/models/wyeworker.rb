ac47ef0075e30e49eb3b22a911ec70d4132bffa8
Natalia Schlebinger <natalia.schlebinger@gmail.com>
2024-07-15 11:46:36 -0300

    Whoops, encontre esto con los tests

--- app/models/wyeworker.rb
+++ app/models/wyeworker.rb
@@ -9,7 +9,7 @@
 #
 class Wyeworker < ApplicationRecord
   has_many :wyeworker_initiative_belongings
-  has_many :initiatives, through: :wyeworker_initiative_belonging
+  has_many :initiatives, through: :wyeworker_initiative_belongings
 
   validates :name, presence: true
 end
