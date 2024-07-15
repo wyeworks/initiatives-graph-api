274a389bdf463304a13d1288a4f306a58974372b
Natalia Schlebinger <natalia.schlebinger@gmail.com>
2024-07-15 14:45:57 -0300

    Formatting

--- spec/models/initiative_spec.rb
+++ spec/models/initiative_spec.rb
@@ -22,11 +22,8 @@
 require 'rails_helper'
 
 RSpec.describe Initiative, type: :model do
-  
   context 'in its entities,' do
-
       it 'may have helpers' do
-
         source = Wyeworker.new(name: "Source Pablo")
         initiative_no_helpers = Initiative.new(title: "Test Inititative No Helpers", source: source)
 
@@ -36,20 +33,16 @@ RSpec.describe Initiative, type: :model do
         initiative_with_helper = Initiative.new(title: "Test Inititative With Helper", source: source, helpers: [helper])
 
         expect(initiative_with_helper).to be_valid
-        
       end
 
       it 'must have a source' do
-
         helper = Wyeworker.new(name: "Helper Pedro")
         initiative = Initiative.new(title: "Test Inititative", helpers: [helper])
 
         expect(initiative).to be_invalid
-
       end
 
       it 'may have a parent' do
-
         source = Wyeworker.new(name: "Source Pablo")
         initiative_no_parent = Initiative.new(title: "Test Inititative No Parent", source: source)
 
@@ -59,29 +52,23 @@ RSpec.describe Initiative, type: :model do
         initiative_with_parent = Initiative.new(title: "Test Inititative With Parent", source: source, parent: parent)
 
         expect(initiative_with_parent).to be_valid
-
       end
   end
   
   context 'when looking back from its entities,' do
-
     it 'is an initiative of its source' do
-
       source = Wyeworker.new(name: "Source Pablo")
       initiative = Initiative.create(title: "Test Inititative", source: source)
 
       expect(source.initiatives).to include(initiative)
-
     end
-  
-    it 'is an initiative of its helpers' do
 
+    it 'is an initiative of its helpers' do
       source = Wyeworker.new(name: "Source Pablo")
       helper = Wyeworker.new(name: "Helper Pedro")
       initiative = Initiative.create(title: "Test Inititative", source: source, helpers: [helper])
 
       expect(helper.initiatives).to include(initiative)
-
     end
   end
 
