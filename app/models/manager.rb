88015196faaf851ea4ce5da45034bbc7f10c24d3
Natalia Schlebinger <natalia.schlebinger@gmail.com>
2024-07-16 09:56:40 -0300

    Manager as a subtype of Wyeworker

---
+++ app/models/manager.rb
@@ -0,0 +1,2 @@
+class Manager < Wyeworker
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_10_143546) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_15_224857) do
   create_table "initiatives", force: :cascade do |t|
     t.string "title", null: false
     t.string "description"
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_10_143546) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
   end
 
   add_foreign_key "initiatives", "initiatives", column: "parent_id"
