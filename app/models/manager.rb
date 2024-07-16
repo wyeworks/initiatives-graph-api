e88d43343a74e96d488bc15b65fa76191031f01a
Natalia Schlebinger <natalia.schlebinger@gmail.com>
2024-07-16 09:57:02 -0300

    Annotate Manager

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: wyeworkers
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  type       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Manager < Wyeworker
 end
