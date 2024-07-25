2ce6d42ac12db2bd98de310236fc68c57b7cbcb3
Natalia Schlebinger <natalia.schlebinger@gmail.com>
2024-07-25 14:59:52 -0300

    No need to drop the whole table in the migration

--- db/migrate/20240724140620_add_delete_cascade_for_wyeworkers_and_initiatives.rb
+++ db/migrate/20240724140620_add_delete_cascade_for_wyeworkers_and_initiatives.rb
@@ -3,32 +3,15 @@ class AddDeleteCascadeForWyeworkersAndInitiatives < ActiveRecord::Migration[7.1]
     reversible do |direction|
       change_table :wyeworker_initiative_belongings do
         direction.up do
-          drop_table :wyeworker_initiative_belongings,
-                     primary_key: %i[wyeworker_id initiative_id] do |t|
-            t.string :kind
-            t.belongs_to :initiative, null: false, foreign_key: true
-            t.belongs_to :wyeworker, null: false, foreign_key: true
-          end
-          create_table :wyeworker_initiative_belongings,
-                       primary_key: %i[wyeworker_id initiative_id] do |t|
-            t.string :kind
-            t.belongs_to :initiative, null: false, foreign_key: { on_delete: :cascade }
-            t.belongs_to :wyeworker, null: false, foreign_key: { on_delete: :cascade }
-          end
+          remove_foreign_key :wyeworker_initiative_belongings, :initiatives
+          add_foreign_key :wyeworker_initiative_belongings,
+                          :initiatives,
+                          null: false,
+                          foreign_key: { on_delete: :cascade }
         end
         direction.down do
-          drop_table :wyeworker_initiative_belongings,
-                     primary_key: %i[wyeworker_id initiative_id] do |t|
-            t.string :kind
-            t.belongs_to :initiative, null: false, foreign_key: { on_delete: :cascade }
-            t.belongs_to :wyeworker, null: false, foreign_key: { on_delete: :cascade }
-          end
-          create_table :wyeworker_initiative_belongings,
-                       primary_key: %i[wyeworker_id initiative_id] do |t|
-            t.string :kind
-            t.belongs_to :initiative, null: false, foreign_key: true
-            t.belongs_to :wyeworker, null: false, foreign_key: true
-          end
+          remove_foreign_key :wyeworker_initiative_belongings, :initiatives
+          add_foreign_key :wyeworker_initiative_belongings, :initiatives, null: false
         end
       end
     end
