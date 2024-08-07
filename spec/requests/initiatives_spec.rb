8d3009f3d606015cda6b45d439d474d6298dc45f
Natalia Schlebinger <natalia.schlebinger@gmail.com>
2024-08-07 16:11:03 -0300

    Good name for test examples

--- spec/requests/initiatives_spec.rb
+++ spec/requests/initiatives_spec.rb
@@ -6,17 +6,17 @@ RSpec.describe "Initiatives Endpoint", type: :request do
   let(:initiative_titles) { initiatives.map(&:title) }
   let(:developer) { create(:wyeworker) }
 
-  it "shows all initiatives" do
+  it "GET /initiatives" do
     get "/initiatives"
     expect(response.body).to include(*initiative_titles)
   end
 
-  it "singles out one initiative" do
+  it "GET /initiatives/:initiative_id" do
     get "/initiatives/#{initiatives.first.id}"
     expect(response.body).to include(initiatives.first.description)
   end
 
-  it "creates an initiative" do
+  it "POST /initiatives" do
     i = build(:initiative, source: create(:manager), helpers: create_list(:wyeworker, 3))
 
     post "/initiatives", params: { initiative: i.as_param, source: i.source_param, helpers: i.helpers_param }
@@ -24,7 +24,7 @@ RSpec.describe "Initiatives Endpoint", type: :request do
     expect(response.body).to include(i.title)
   end
 
-  it "updates an initiative" do
+  it "PUT /initiatives" do
     different_title = "A different initiative title"
 
     put "/initiatives/#{initiatives.first.id}",
@@ -33,7 +33,7 @@ RSpec.describe "Initiatives Endpoint", type: :request do
     expect(response.body).to include(different_title)
   end
 
-  it "deletes an initiative" do
+  it "DELETE /initiatives/:initiative_id" do
     delete "/initiatives/#{initiatives.first.id}"
     expect(response).to have_http_status(:no_content)
   end
