0f3b7f0a6895813e9f33fa15d068c56f8f020414
Natalia Schlebinger <natalia.schlebinger@gmail.com>
2024-08-22 14:42:57 -0300

    Fix up migration

--- db/migrate/20240809125904_separate_source_and_helpers_rel.rb
+++ db/migrate/20240809125904_separate_source_and_helpers_rel.rb
@@ -2,19 +2,19 @@
 
 class SeparateSourceAndHelpersRel < ActiveRecord::Migration[7.1]
   def change
+    # add helpers table
+    create_table :initiatives_wyeworkers,
+                 primary_key: %i[wyeworker_id initiative_id] do |t|
+      t.references :initiative, null: false, foreign_key: { on_delete: :cascade }
+      t.references :wyeworker, null: false, foreign_key: { on_delete: :cascade }
+      t.timestamps
+    end
+
+    # add source column
+    add_reference :initiatives, :owner, null: false, foreign_key: { to_table: :wyeworkers }
+
     reversible do |direction|
       direction.up do
-        # add helpers table
-        create_table :initiatives_wyeworkers,
-                     primary_key: %i[wyeworker_id initiative_id] do |t|
-          t.references :initiative, null: false, foreign_key: { on_delete: :cascade }
-          t.references :wyeworker, null: false, foreign_key: { on_delete: :cascade }
-          t.timestamps
-        end
-
-        # add source column
-        add_reference :initiatives, :owner, null: false, foreign_key: { to_table: :wyeworkers }
-
         # drop weird table
         drop_table :wyeworker_initiative_belongings
       end
@@ -27,12 +27,6 @@ class SeparateSourceAndHelpersRel < ActiveRecord::Migration[7.1]
           t.belongs_to :wyeworker, null: false, foreign_key: { on_delete: :cascade }
           t.timestamps
         end
-
-        # drop helpers table
-        drop_table :initiatives_wyeworkers
-
-        # drop source column
-        remove_column :initiatives, :owner_id
       end
     end
   end
