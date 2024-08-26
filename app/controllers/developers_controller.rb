39eca8fc081213adae9a6fb6b9b18528628dd3e1
Natalia Schlebinger <natalia.schlebinger@gmail.com>
2024-08-26 10:45:20 -0300

    No metaprogramming for wyeworkers controllers

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,5 +1,49 @@
 # frozen_string_literal: true
 
-class DevelopersController < WyeworkersController
-  WyeworkerKind = Developer
+class DevelopersController < ApplicationController
+  before_action :set_developer, only: %i[show update destroy]
+
+  def index
+    render json: Developer.all
+  end
+
+  def show
+    render json: @developer
+  end
+
+  # POST
+  def create
+    @developer = Developer.new(developer_params)
+    if @developer.save
+      render json: @developer, status: :created, location: @developer
+    else
+      render json: @developer.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT
+  def update
+    if @developer.update(developer_params)
+      render json: @developer
+    else
+      render json: @developer.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE
+  def destroy
+    @developer.destroy
+  end
+
+  private
+
+  def developer_params
+    params
+      .require(:developer)
+      .permit(:name, :id, owner_initiatives: [], helped_initiatives: [])
+  end
+
+  def set_developer
+    @developer = Developer.find(params[:id])
+  end
 end
--- app/controllers/managers_controller.rb
+++ app/controllers/managers_controller.rb
@@ -1,5 +1,49 @@
 # frozen_string_literal: true
 
-class ManagersController < WyeworkersController
-  WyeworkerKind = Manager
+class ManagersController < ApplicationController
+  before_action :set_manager, only: %i[show update destroy]
+
+  def index
+    render json: Manager.all
+  end
+
+  def show
+    render json: @manager
+  end
+
+  # POST
+  def create
+    @manager = Manager.new(manager_params)
+    if @manager.save
+      render json: @manager, status: :created, location: @manager
+    else
+      render json: @manager.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT
+  def update
+    if @manager.update(manager_params)
+      render json: @manager
+    else
+      render json: @manager.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE
+  def destroy
+    @manager.destroy
+  end
+
+  private
+
+  def manager_params
+    params
+      .require(:manager)
+      .permit(:name, :id, owner_initiatives: [], helped_initiatives: [])
+  end
+
+  def set_manager
+    @manager = Manager.find(params[:id])
+  end
 end
